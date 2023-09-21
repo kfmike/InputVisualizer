2bbdd63a0779338866fbea5297b10ffc57172633
Mike <michael.lefrancois.jr@gmail.com>
2023-09-21 18:17:33 -0400

    initial usb2snes code

---
+++ UI/Usb2SnesGameChangedEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace InputVisualizer.UI
+{
+    public class Usb2SnesGameChangedEventArgs : EventArgs
+    {
+        public string Game { get; set; }
+    }
+}
---
+++ Usb2Snes/Usb2SnesButtonFlags.cs
@@ -0,0 +1,25 @@
+﻿
+namespace InputVisualizer.Usb2Snes
+{
+    public enum Usb2SnesButtonFlags1
+    {
+        None = 0,
+        A = 128,
+        X = 64,
+        L = 32,
+        R = 16
+    }
+
+    public enum Usb2SnesButtonFlags2
+    {
+        None = 0,
+        Up = 8,
+        Down = 4,
+        Left = 2,
+        Right = 1,
+        Start = 16,
+        Select = 32,
+        B = 128,
+        Y = 64,
+    }
+}
