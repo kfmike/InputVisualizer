f392f7ab70a5a84306931c7aa6335ed1c518ef13
Mike <michael.lefrancois.jr@gmail.com>
2024-03-21 17:04:17 -0400

    add general settings and usb2snes server and port config

---
+++ Config/GeneralSettings.cs
@@ -0,0 +1,10 @@
+﻿using InputVisualizer.Usb2Snes;
+
+namespace InputVisualizer.Config
+{
+    public class GeneralSettings
+    {
+        public string Usb2SnesServer { get; set; } = Usb2SnesClient.DEFAULT_SERVER;
+        public string Usb2SnesPort { get; set; } = Usb2SnesClient.DEFAULT_PORT;
+    }
+}
